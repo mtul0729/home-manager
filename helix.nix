67c7cdc18a9e4f94d13f4443c88f5655424845c8
huangcpp <min@max.none>
2024-06-18 23:49:03 +0800

    config helix

--- helix.nix
+++ helix.nix
@@ -1,10 +1,39 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    extraPackages = with pkgs; [nil nixpkgs-fmt];
+    extraPackages = with pkgs; [
+      alejandra
+      clang-tools
+      marksman
+      nil
+      # bash-language-server
+      # nodePackages.vscode-css-languageserver-bin
+      vscode-langservers-extracted
+      shellcheck
+    ];
     settings = {
       theme = "fleet_dark";
+    };
+    languages = {
+      language-server.rust-analyzer.config.check = {command = "clippy";};
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter = {command = "alejandra";};
+          language-servers = ["nil"];
+        }
+        {
+          name = "rust";
+          auto-format = true;
+          formatter = {command = "rustfmt";};
+        }
+      ];
     };
   };
 }
