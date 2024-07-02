fcdb8ba92c345aaa11483f601160d703a07fb929
Myul <myul@foxmail.com>
2024-07-02 16:56:40 +0800

    add fonts

---
+++ fonts.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  # all fonts are linked to /nix/var/nix/profiles/system/sw/share/X11/fonts
+  fonts = {
+    fontconfig.enable = true;
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
+    fontconfig.defaultFonts = {
+      serif = ["Source Han Serif SC" "Source Han Serif TC" "Noto Color Emoji"];
+      sansSerif = ["Source Han Sans SC" "Source Han Sans TC" "Noto Color Emoji"];
+      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
+}
--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   imports = [
     ./shell.nix
     ./helix.nix
+    ./fonts.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -32,22 +33,38 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    # icon fonts
+    material-design-icons
+    font-awesome
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # Noto 系列字体是 Google 主导的，名字的含义是「没有豆腐」（no tofu），因为缺字时显示的方框或者方框被叫作 tofu
+    # Noto 系列字族名只支持英文，命名规则是 Noto + Sans 或 Serif + 文字名称。
+    # 其中汉字部分叫 Noto Sans/Serif CJK SC/TC/HK/JP/KR，最后一个词是地区变种。
+    # noto-fonts # 大部分文字的常见样式，不包含汉字
+    # noto-fonts-cjk # 汉字部分
+    noto-fonts-emoji # 彩色的表情符号字体
+    # noto-fonts-extra # 提供额外的字重和宽度变种
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # 思源系列字体是 Adobe 主导的。其中汉字部分被称为「思源黑体」和「思源宋体」，是由 Adobe + Google 共同开发的
+    source-sans # 无衬线字体，不含汉字。字族名叫 Source Sans 3 和 Source Sans Pro，以及带字重的变体，加上 Source Sans 3 VF
+    source-serif # 衬线字体，不含汉字。字族名叫 Source Code Pro，以及带字重的变体
+    source-han-sans # 思源黑体
+    source-han-serif # 思源宋体
+
+    # nerdfonts
+    # https://github.com/NixOS/nixpkgs/blob/nixos-23.11/pkgs/data/fonts/nerdfonts/shas.nix
+    (nerdfonts.override {
+      fonts = [
+        # symbols icon only
+        "NerdFontsSymbolsOnly"
+        # Characters
+        "FiraCode"
+        "JetBrainsMono"
+        "Iosevka"
+      ];
+    })
+    julia-mono
+    dejavu_fonts
     nil
     # nixpkgs-fmt
     alejandra
