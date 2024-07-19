56e2bdf8c8af099f0ba2d42abd92b0e5f8de210e
mcp <hjp1279@outlook.com>
2024-07-19 15:17:06 +0800

    add cache mirror

--- home.nix
+++ home.nix
@@ -10,6 +10,29 @@
     ./helix.nix
     ./fonts.nix
   ];
+  nix.package = pkgs.nix;
+  nix.settings = {
+    # given the users in this list the right to specify additional substituters via:
+    #    1. `nixConfig.substituters` in `flake.nix`
+    #    2. command line args `--options substituters http://xxx`
+    trusted-users = ["myul"];
+    # extra-substituters = [
+    #   "https://mirror.sjtu.edu.cn/nix-channels/store"
+    #   "https://helix.cachix.org"
+    # ];
+    substituters = [
+      # cache mirror located in China
+      # status: https://mirror.sjtu.edu.cn/
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # status: https://mirrors.ustc.edu.cn/status/
+      # "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+      "https://helix.cachix.org"
+    ];
+    trusted-substituters = ["https://helix.cachix.org"];
+    extra-trusted-public-keys = ["helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="];
+    auto-optimise-store = true;
+  };
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "myul";
